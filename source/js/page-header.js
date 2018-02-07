865900e563a6636e6b1ad994b002c695a5d0311f
Anton Lantukh <lantukh@bk.ru>
2018-02-07 14:45:16 +0400

    Поправил схему работы скрипта в шапке

--- source/js/page-header.js
+++ source/js/page-header.js
@@ -3,6 +3,7 @@
 var navList = document.querySelector('.main-nav');
 var headerToggler = document.querySelector('.page-header__toggler');
 var logoContainer = document.querySelector('.page-header__logo-container');
+var pageHeader = document.querySelector('.page-header');
 
 navList.classList.remove('main-nav--nojs');
 logoContainer.classList.remove('page-header__logo-container--nojs');
@@ -13,11 +14,12 @@ headerToggler.addEventListener('click', function() {
       navList.classList.remove('main-nav--closed');
       logoContainer.classList.remove('page-header__logo-container--closed');
       logoContainer.classList.add('page-header__logo-container--opened');
+      pageHeader.classList.add('page-header--static');
     } else {
       navList.classList.add('main-nav--closed');
       navList.classList.remove('main-nav--opened');
-      navList.classList.add('main-nav--closed');
       logoContainer.classList.remove('page-header__logo-container--opened');
       logoContainer.classList.add('page-header__logo-container--closed');
+      pageHeader.classList.remove('page-header--static');
     }
   });
