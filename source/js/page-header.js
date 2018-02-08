1d1de73629e9d46d4a047810d7379e54b23391da
Anton Lantukh <lantukh@bk.ru>
2018-02-08 19:21:48 +0400

    Добавляю свг графику

--- source/js/page-header.js
+++ source/js/page-header.js
@@ -4,6 +4,8 @@ var navList = document.querySelector('.main-nav');
 var headerToggler = document.querySelector('.page-header__toggler');
 var logoContainer = document.querySelector('.page-header__logo-container');
 var pageHeader = document.querySelector('.page-header');
+var iconBurger= document.querySelector('.page-header__burger');
+var iconCross = document.querySelector('.page-header__cross');
 
 navList.classList.remove('main-nav--nojs');
 logoContainer.classList.remove('page-header__logo-container--nojs');
@@ -15,11 +17,15 @@ headerToggler.addEventListener('click', function() {
       logoContainer.classList.remove('page-header__logo-container--closed');
       logoContainer.classList.add('page-header__logo-container--opened');
       pageHeader.classList.add('page-header--static');
+      iconBurger.classList.add('page-header__toggler--icon-change');
+      iconCross.classList.remove('page-header__toggler--icon-change');
     } else {
       navList.classList.add('main-nav--closed');
       navList.classList.remove('main-nav--opened');
       logoContainer.classList.remove('page-header__logo-container--opened');
       logoContainer.classList.add('page-header__logo-container--closed');
       pageHeader.classList.remove('page-header--static');
+      iconBurger.classList.remove('page-header__toggler--icon-change');
+      iconCross.classList.add('page-header__toggler--icon-change');
     }
   });
